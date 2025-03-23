b2bba0ea99af8c27e12225423c6705051b97583f
NinzBanroad <73373204+NinzBanroad@users.noreply.github.com>
2025-03-24 05:20:02 +0800

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import { LOGOUT } from '../actions/types';
 
 // Create an instance of axios
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://phonebook-api-4cuk.onrender.com/api',
 });
 
 // Request interceptor for adding token to headers and configuring content type
